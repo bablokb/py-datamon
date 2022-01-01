b6ba365e6a754e7fb440e313bd0369b06d593c21
Bernhard Bablok <bablokb@gmx.de>
2022-01-01 07:56:07 +0100

    added configuration classes

--- files/usr/local/lib/py-datamon/lib/__init__.py
+++ files/usr/local/lib/py-datamon/lib/__init__.py
@@ -1,1 +1,16 @@
-from . DMPlot import DMPlot as DMPlot
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Application module for py-datamon. This file just imports all classes
+# into the namespace of the module.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/py-datamon
+#
+# ----------------------------------------------------------------------------
+
+from . DMPlot          import DMPlot          as DMPlot
+from . DMConfigPlot    import DMConfigPlot    as DMConfigPlot
+from . DMConfigSubplot import DMConfigSubplot as DMConfigSubplot
+from . DMConfigValue   import DMConfigValue   as DMConfigValue
