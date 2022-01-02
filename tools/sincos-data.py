4967dd1651d2ac7e1f19cd323d613cee60579548
Bernhard Bablok <bablokb@gmx.de>
2022-01-02 08:51:28 +0100

    generate additional columns

--- tools/sincos-data.py
+++ tools/sincos-data.py
@@ -11,15 +11,21 @@
 #
 # ----------------------------------------------------------------------------
 
-import sys, time, math
+import sys, time, math, random
+
+random.seed()
 
 delay = float(sys.argv[1]) if len(sys.argv) > 1 else 0
-y = [None,None]
+y     = [None for n in range(6)]
+t0    = time.perf_counter()
 
-t0 = time.perf_counter()
 while True:
   t = time.perf_counter() - t0
-  y[0] = str(math.sin(t))
-  y[1] = str( math.cos(t))
-  print("%f,%s" % (t,",".join(y)),flush=True)
+  y[0] = math.sin(t)
+  y[1] = math.cos(t)
+  y[2] = y[0] + y[1]
+  y[3] = y[0] + random.normalvariate(0,1)
+  y[4] = y[1] + random.normalvariate(0,1)
+  y[5] = y[3] + y[4]
+  print("%f,%s" % (t,",".join(map(str,y))),flush=True)
   time.sleep(delay)
