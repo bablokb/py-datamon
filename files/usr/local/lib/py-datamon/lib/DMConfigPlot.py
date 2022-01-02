b5eda87dd6ebc54f60089619d07a2fa45b89133f
Bernhard Bablok <bablokb@gmx.de>
2022-01-02 07:28:15 +0100

    calculate attribute 'rows'

--- files/usr/local/lib/py-datamon/lib/DMConfigPlot.py
+++ files/usr/local/lib/py-datamon/lib/DMConfigPlot.py
@@ -9,18 +9,23 @@
 #
 # ----------------------------------------------------------------------------
 
-import types
+import types, math
 from lib import DMConfigSubplot
 
 # --- configuration-object for plots   ---------------------------------------
 
 class DMConfigPlot(types.SimpleNamespace):
+
+  # --- constructor   --------------------------------------------------------
+
   def __init__(self,app,conf):
+    """ constructor """
 
     self.msg = app.msg
     
     # set defaults
     self.title = "Default Title"
+    self.cols  = 1
 
     # override with data from config-file
     super(DMConfigPlot,self).__init__(**conf)
@@ -29,3 +34,13 @@ class DMConfigPlot(types.SimpleNamespace):
     self.msg("DMConfigPlot: parsing config for %d subplots" % len(self.plots))
     self._subplots = [DMConfigSubplot(app,plot) for plot in self.plots]
     del self.plots
+
+    self._get_layout()
+
+  # --- calculate layout   ---------------------------------------------------
+
+  def _get_layout(self):
+    """ calculate layout """
+
+    if not hasattr(self,"rows"):
+      self.rows = math.ceil(len(self._subplots)/self.cols)
