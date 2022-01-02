7f9bdc0cd43bcdbffe32edf83a8041d06f1614d2
Bernhard Bablok <bablokb@gmx.de>
2022-01-02 09:01:01 +0100

    added count-parameter

--- tools/sincos-data.py
+++ tools/sincos-data.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# This is a simple data-generator for test-data. You can pass a delay as
-# first argument. The default delay is 0 seconds.
+# This is a simple data-generator for test-data. You can pass a delay and a
+# count to the script. The default delay is 0 seconds, the default count is
+# unlimited
 #
 # Author: Bernhard Bablok
 # License: GPL3
@@ -16,6 +17,8 @@ import sys, time, math, random
 random.seed()
 
 delay = float(sys.argv[1]) if len(sys.argv) > 1 else 0
+count = int(sys.argv[2])   if len(sys.argv) > 2 else 0
+i     = count
 y     = [None for n in range(6)]
 t0    = time.perf_counter()
 
@@ -28,4 +31,7 @@ while True:
   y[4] = y[1] + random.normalvariate(0,1)
   y[5] = y[3] + y[4]
   print("%f,%s" % (t,",".join(map(str,y))),flush=True)
+  i -= 1
+  if i == 0:
+    break
   time.sleep(delay)
