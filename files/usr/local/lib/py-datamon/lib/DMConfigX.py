21d8e2e219168087e13b1ab0638835e0c7c318a5
Bernhard Bablok <bablokb@gmx.de>
2022-01-13 20:22:57 +0100

    initial commit

---
+++ files/usr/local/lib/py-datamon/lib/DMConfigX.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Class DMConfigX: configuration data for x-value
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/py-datamon
+#
+# ----------------------------------------------------------------------------
+
+import types
+
+# --- configuration-object for x-value   -------------------------------------
+
+class DMConfigX(types.SimpleNamespace):
+  def __init__(self,app,conf):
+
+    self.msg = app.msg
+
+    # set defaults
+    self.col       = 0
+    self.label     = "time"
+    self.normalize = False
+    self.scale     = 1
+
+    # override with data from config-file
+    super(DMConfigX,self).__init__(**conf)
